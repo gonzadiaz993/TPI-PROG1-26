74d6ff04c4728f666df801da2bbc0380ab6ae16a
gonzadiaz993 <gonza_diaz993@hotmail.com>
2026-05-12 12:13:01 -0300

    menu definido
    menu definido

---
+++ tpi_fcn.py
@@ -0,0 +1,12 @@
+import csv
+def menu():
+    print("""
+1) Agregar país.
+2) Actualizar datos.
+3) Buscar pais por nombre.
+4) Filtrar países.
+5) Ordenar países.
+6) Mostrar estadísticas.
+7) Salir.
+          """)
+
---
+++ tpi_prog1.py
@@ -0,0 +1,24 @@
+from tpi_fcn import*
+paises = []
+
+while True:
+    menu()
+    opcion = input("Elija la opcion que desea ingresar: ")
+    match opcion:
+        case "1":
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
+        case "4":
+            pass
+        case "5":
+            pass
+        case "6":
+            pass
+        case "7":
+            print("Fin del programa. Hasta luego.")
+            break
+        case _:
+            print("Opcion incorrecta. Intente nuevamente")
